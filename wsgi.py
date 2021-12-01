a3a534abec03d2673d8aea6382462ab7c033b6bb
carloleo <carlo_leo97@outlook.it>
2021-12-01 14:36:46 +0100

    initial commit

---
+++ wsgi.py
@@ -0,0 +1,13 @@
+"""
+Message in a Bottle
+Web Server Gateway Interface
+This file is the entry point for
+mib-users-ms microservice.
+"""
+from mib import create_app
+
+# application instance
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
