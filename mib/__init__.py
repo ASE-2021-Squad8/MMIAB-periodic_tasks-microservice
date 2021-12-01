9e4ae7fb9c726c61c0377e978d26b5983413a206
carloleo <carlo_leo97@outlook.it>
2021-12-01 21:19:09 +0100

    got coverage and removed unsed code

--- mib/__init__.py
+++ mib/__init__.py
@@ -41,10 +41,10 @@ def create_app():
         config_object = "config.DevConfig"
     elif flask_env == "testing":
         config_object = "config.TestConfig"
-    elif flask_env == "production":
+    elif flask_env == "production":  # pragma: no cover
         config_object = "config.ProdConfig"
     else:
-        raise RuntimeError(
+        raise RuntimeError(  # pragma: no cover
             "%s is not recognized as valid app environment. You have to setup the environment!"
             % flask_env
         )
@@ -68,25 +68,8 @@ def init_logger():
     logger.addHandler(default_handler)
 
 
-def register_specifications(_api_app):
-    """
-    This function registers all resources in the flask application
-    :param _api_app: Flask Application Object
-    :return: None
-    """
-
-    # we need to scan the specifications package and add all yaml files.
-    from importlib_resources import files
-
-    folder = files("mib.specifications")
-    for _, _, files in os.walk(folder):
-        for file in files:
-            if file.endswith(".yaml") or file.endswith(".yml"):
-                file_path = folder.joinpath(file)
-                _api_app.add_api(file_path)
-
-
-def create_celery(flask_app):
+#   period tasks are tested functionally
+def create_celery(flask_app):  # pragma: no cover
     # broker's url and storing results
     BACKEND = BROKER = "redis://redis:6379"
 
--- mib/background.py
+++ mib/background.py
@@ -1,9 +1,5 @@
+# pragma: no cover
 from mib import create_app, create_celery
 
 flask_app = create_app()
 app = create_celery(flask_app)
-
-try:
-    import mib.tasks
-except ImportError:
-    raise RuntimeError("Cannot import celery tasks")
